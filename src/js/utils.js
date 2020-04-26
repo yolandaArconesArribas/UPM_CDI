ad1e324818699606ecf1e41777cdf63c464ea1d4
kevin dev1 <kv1ncq@gmail.com>
2020-04-26 18:58:54 +0200

    terminado storage

---
+++ src/js/utils.js
@@ -0,0 +1,126 @@
+var data_user = [
+    {
+        "Usuarios": [
+            {
+                "_id": "5ea44db0d23027d697f458b7",
+                "Nombre": "Wright",
+                "Apellido": "Love",
+                "DNI": "26434958S",
+                "CodigoPostal": 1875,
+                "Localidad": "Moncloa",
+                "Telefono": "+34 (856) 511-2931",
+                "Email": "wrightlove@hawkster.com",
+                "TarjetaFidelizacion": "9713T",
+                "PuntosAcumulados": 415
+            },
+            {
+                "_id": "5ea44db03554332a4968c425",
+                "Nombre": "Sears",
+                "Apellido": "Hill",
+                "DNI": "12642470S",
+                "CodigoPostal": 1552,
+                "Localidad": "Moncloa",
+                "Telefono": "+34 (873) 453-3979",
+                "Email": "searshill@hawkster.com",
+                "TarjetaFidelizacion": "2372B",
+                "PuntosAcumulados": 53
+            },
+            {
+                "_id": "5ea44db091bbfbb117afd6c3",
+                "Nombre": "Rowena",
+                "Apellido": "Richard",
+                "DNI": "35819135T",
+                "CodigoPostal": 1589,
+                "Localidad": "Madrid",
+                "Telefono": "+34 (889) 449-2390",
+                "Email": "rowenarichard@hawkster.com",
+                "TarjetaFidelizacion": "3744A",
+                "PuntosAcumulados": 130
+            },
+            {
+                "_id": "5ea44db0283f962615c22e33",
+                "Nombre": "Lewis",
+                "Apellido": "Clarke",
+                "DNI": "44586078A",
+                "CodigoPostal": 1193,
+                "Localidad": "Aravaca",
+                "Telefono": "+34 (866) 488-2559",
+                "Email": "lewisclarke@hawkster.com",
+                "TarjetaFidelizacion": "8705C",
+                "PuntosAcumulados": 17
+            },
+            {
+                "_id": "5ea44db02d4a6f60d31dada4",
+                "Nombre": "Tiffany",
+                "Apellido": "Park",
+                "DNI": "98334656T",
+                "CodigoPostal": 1266,
+                "Localidad": "Atocha",
+                "Telefono": "+34 (847) 585-3964",
+                "Email": "tiffanypark@hawkster.com",
+                "TarjetaFidelizacion": "5637T",
+                "PuntosAcumulados": 3
+            },
+            {
+                "_id": "5ea44db0295a115b38c5d7fc",
+                "Nombre": "Josephine",
+                "Apellido": "Gordon",
+                "DNI": "48393394B",
+                "CodigoPostal": 1589,
+                "Localidad": "Majadahonda",
+                "Telefono": "+34 (899) 597-3018",
+                "Email": "josephinegordon@hawkster.com",
+                "TarjetaFidelizacion": "5624A",
+                "PuntosAcumulados": 3
+            },
+            {
+                "_id": "5ea44db03af9d28a10d37ceb",
+                "Nombre": "Katharine",
+                "Apellido": "Ford",
+                "DNI": "21482528T",
+                "CodigoPostal": 1227,
+                "Localidad": "Madrid",
+                "Telefono": "+34 (889) 460-3170",
+                "Email": "katharineford@hawkster.com",
+                "TarjetaFidelizacion": "9428S",
+                "PuntosAcumulados": 196
+            },
+            {
+                "_id": "5ea44db0a3fa3bb258b7338b",
+                "Nombre": "Eleanor",
+                "Apellido": "Barber",
+                "DNI": "55512820B",
+                "CodigoPostal": 1241,
+                "Localidad": "Aravaca",
+                "Telefono": "+34 (865) 573-2168",
+                "Email": "eleanorbarber@hawkster.com",
+                "TarjetaFidelizacion": "6874B",
+                "PuntosAcumulados": 104
+            },
+            {
+                "_id": "5ea44db0608ac69848646329",
+                "Nombre": "Liz",
+                "Apellido": "Macias",
+                "DNI": "63555279T",
+                "CodigoPostal": 1418,
+                "Localidad": "Majadahonda",
+                "Telefono": "+34 (866) 599-3200",
+                "Email": "lizmacias@hawkster.com",
+                "TarjetaFidelizacion": "5715A",
+                "PuntosAcumulados": 382
+            },
+            {
+                "_id": "5ea44db02a9e3a345590a0f2",
+                "Nombre": "Arlene",
+                "Apellido": "Beard",
+                "DNI": "74389872B",
+                "CodigoPostal": 1686,
+                "Localidad": "Atocha",
+                "Telefono": "+34 (822) 597-3700",
+                "Email": "arlenebeard@hawkster.com",
+                "TarjetaFidelizacion": "9257S",
+                "PuntosAcumulados": 7
+            }
+        ]
+    }
+];
